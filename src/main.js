cfbbb407e04169d4534354d783e0eecc7e94f9da
juli4nz <1939392+juli4nz@users.noreply.github.com>
2020-07-23 17:09:44 +0100

    Add analytics

--- src/main.js
+++ src/main.js
@@ -3,11 +3,20 @@ import App from './App.vue';
 import router from './router';
 import vSelect from 'vue-select';
 import 'vue-select/dist/vue-select.css';
+import VueGtag from 'vue-gtag';
 
 Vue.config.productionTip = false;
 Vue.component('v-select', vSelect);
 
+Vue.use(
+   VueGtag,
+   {
+      config: { id: 'UA-105987352-6' },
+   },
+   router
+);
+
 new Vue({
-  router,
-  render: (h) => h(App),
+   router,
+   render: (h) => h(App),
 }).$mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -9,86 +9,86 @@ import EmentaChef from '@/components/EmentaChef.vue';
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/ementa/905/omoinho',
-  },
-  {
-    path: '/ementa/:id(\\d+)/:slug',
-    component: Ementa,
-    children: [
-      // Home
-      {
-        path: '/',
-        name: 'home',
-        component: EmentaTopics,
-      },
-      // Categories
-      {
-        path: ':a_slug',
-        name: 'home_a',
-        component: EmentaTopics,
-      },
-      {
-        path: ':a_slug/:b_slug',
-        name: 'home_b',
-        component: EmentaTopics,
-      },
-      {
-        path: ':a_slug/:b_slug/:c_slug',
-        name: 'home_c',
-        component: EmentaTopics,
-      },
-      // Details
-      {
-        path: ':a_slug/detail',
-        name: 'home_a_detail',
-        component: EmentaDetail,
-      },
-      {
-        path: ':a_slug/:b_slug/detail',
-        name: 'home_b_detail',
-        component: EmentaDetail,
-      },
-      {
-        path: ':a_slug/:b_slug/:c_slug/detail',
-        name: 'home_c_detail',
-        component: EmentaDetail,
-      },
-      // Chef
-      {
-        path: 'chef_suggestions',
-        name: 'chef',
-        component: EmentaChef,
-      },
-      // Catalog
-      {
-        path: ':a_slug/catalog',
-        name: 'home_a_catalog',
-        component: EmentaCatalog,
-      },
-      {
-        path: ':a_slug/:b_slug/catalog',
-        name: 'home_b_catalog',
-        component: EmentaCatalog,
-      },
-      {
-        path: ':a_slug/:b_slug/:c_slug/catalog',
-        name: 'home_c_catalog',
-        component: EmentaCatalog,
-      },
-    ],
-  },
+   {
+      path: '/',
+      redirect: '/ementa/905/omoinho',
+   },
+   {
+      path: '/ementa/:id(\\d+)/:slug',
+      component: Ementa,
+      children: [
+         // Home
+         {
+            path: '/',
+            name: 'home',
+            component: EmentaTopics,
+         },
+         // Categories
+         {
+            path: ':a_slug',
+            name: 'home_a',
+            component: EmentaTopics,
+         },
+         {
+            path: ':a_slug/:b_slug',
+            name: 'home_b',
+            component: EmentaTopics,
+         },
+         {
+            path: ':a_slug/:b_slug/:c_slug',
+            name: 'home_c',
+            component: EmentaTopics,
+         },
+         // Details
+         {
+            path: ':a_slug/detail',
+            name: 'home_a_detail',
+            component: EmentaDetail,
+         },
+         {
+            path: ':a_slug/:b_slug/detail',
+            name: 'home_b_detail',
+            component: EmentaDetail,
+         },
+         {
+            path: ':a_slug/:b_slug/:c_slug/detail',
+            name: 'home_c_detail',
+            component: EmentaDetail,
+         },
+         // Chef
+         {
+            path: 'chef_suggestions',
+            name: 'chef',
+            component: EmentaChef,
+         },
+         // Catalog
+         {
+            path: ':a_slug/catalog',
+            name: 'home_a_catalog',
+            component: EmentaCatalog,
+         },
+         {
+            path: ':a_slug/:b_slug/catalog',
+            name: 'home_b_catalog',
+            component: EmentaCatalog,
+         },
+         {
+            path: ':a_slug/:b_slug/:c_slug/catalog',
+            name: 'home_c_catalog',
+            component: EmentaCatalog,
+         },
+      ],
+   },
 ];
 
 const router = new VueRouter({
-  routes,
-  mode: 'history',
+   routes,
+   mode: 'history',
 });
 
 router.beforeEach((to, from, next) => {
-  window.scrollTo(0, 0);
-  next();
+   window.scrollTo(0, 0);
+   next();
 });
 
 export default router;
